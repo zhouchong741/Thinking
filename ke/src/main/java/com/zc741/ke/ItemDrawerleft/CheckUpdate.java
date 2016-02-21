4fdb195be8f5d07971c7ea361b862fbbcf0a1c19
zhouchong741 <zhouchong741@gmail.com>
2016-02-21 12:08:43 +0800

    new

---
+++ ke/src/main/java/com/zc741/ke/ItemDrawerleft/CheckUpdate.java
@@ -0,0 +1,13 @@
+package com.zc741.ke.ItemDrawerleft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_update);
+    }
+}
---
+++ ke/src/main/java/com/zc741/ke/ItemDrawerleft/IntroduceActivity.java
@@ -0,0 +1,15 @@
+package com.zc741.ke.ItemDrawerleft;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zc741.ke.R;
+
+public class IntroduceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduce);
+    }
+}

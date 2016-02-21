903e569ac9d368539f9812690dcb69e5249294fe
zhouchong741 <zhouchong741@gmail.com>
2016-02-21 12:08:35 +0800

    new

---
+++ ke/src/main/java/com/zc741/ke/ItemDrawerleft/SuggestionActivity.java
@@ -0,0 +1,13 @@
+package com.zc741.ke.ItemDrawerleft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggestion);
+    }
+}
